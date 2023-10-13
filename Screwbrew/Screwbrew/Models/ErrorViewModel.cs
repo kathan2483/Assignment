8e38de7dc1d07982a4563118333a7616b6f3968a
kathan2483 <68279721+kathan2483@users.noreply.github.com>
2023-10-13 13:18:43 -0400

    Add files via upload

---
+++ Screwbrew/Screwbrew/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Screwbrew.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
